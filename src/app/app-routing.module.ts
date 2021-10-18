0dbbe4165699f1905c5d0f9d37a6e3f6e8a0943d
yeah568 <awoo@dewdrop.dog>
2021-10-17 20:29:51 -0700

    add finish component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FinishComponent } from './finish/finish.component';
 import { ToastComponent } from './task/toast/toast.component';
 import { WorldmapComponent } from './worldmap/worldmap.component';
 
 const routes: Routes = [
   {path: 'worldmap', component: WorldmapComponent},
-  {path: 'task/toast', component: ToastComponent}
+  {path: 'task/toast', component: ToastComponent},
+  {path: 'finish', component: FinishComponent},
 ];
 
 @NgModule({
