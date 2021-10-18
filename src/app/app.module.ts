4fed67b8688de127b391b35161c1d3ca693a7428
yeah568 <awoo@dewdrop.dog>
2021-10-17 19:21:19 -0700

    add inventory service + component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldmapComponent } from './worldmap/worldmap.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorldmapComponent
+    WorldmapComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/inventory/inventory.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../inventory.service';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit {
+
+  constructor(private readonly inventoryService: InventoryService) {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
