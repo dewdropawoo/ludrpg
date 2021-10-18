e737ad4ebdf3e159b10975f11d3d7bdc10410e47
yeah568 <awoo@dewdrop.dog>
2021-10-17 19:15:13 -0700

    init+worldmap init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorldmapComponent } from './worldmap/worldmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldmapComponent
   ],
   imports: [
     BrowserModule,
