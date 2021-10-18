502faeb0246cb78176dae45b792ef18ed8f5e688
yeah568 <awoo@dewdrop.dog>
2021-10-18 15:11:02 -0700

    add some styling for the overall game

--- src/app/inventory.service.ts
+++ src/app/inventory.service.ts
@@ -16,20 +16,20 @@ export class InventoryService {
   allItems: Map<string, Item> = new Map([
     ['amogus', {
       name: 'amogus',
-      description: 'amogus',
-      imgUrl: 'https://via.placeholder.com/64x64',
+      description: 'much longer description. we put words here to make the description bigger.',
+      imgUrl: 'https://via.placeholder.com/128x128',
       obtained: false,
     }],
     ['map', {
       name: 'map',
       description: 'map',
-      imgUrl: 'https://via.placeholder.com/64x64',
+      imgUrl: 'https://via.placeholder.com/128x128',
       obtained: false,
     }],
     ['aoc', {
       name: 'aoc',
       description: 'aoc',
-      imgUrl: 'https://via.placeholder.com/64x64',
+      imgUrl: 'https://via.placeholder.com/128x128',
       obtained: false,
     }],
   ]);
