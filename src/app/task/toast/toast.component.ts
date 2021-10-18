6742e1c647854a88d939eb2750b1c4032da36103
yeah568 <awoo@dewdrop.dog>
2021-10-17 20:36:54 -0700

    navigate to worldmap on question answer

--- src/app/task/toast/toast.component.ts
+++ src/app/task/toast/toast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InventoryService } from 'src/app/inventory.service';
 
 @Component({
@@ -9,17 +10,19 @@ import { InventoryService } from 'src/app/inventory.service';
 export class ToastComponent implements OnInit {
   // TODO: define question data here, and choose randomly to render
 
-  constructor(private readonly inventoryService: InventoryService) { }
+  constructor(private readonly inventoryService: InventoryService, private readonly router: Router) { }
 
   ngOnInit(): void {
   }
 
   onCorrect() {
     this.inventoryService.setObtained('amogus', true);
+    this.router.navigate(['/worldmap']);
   }
 
   onIncorrect() {
     this.inventoryService.setObtained('amogus', false);
+    this.router.navigate(['/worldmap']);
   }
 
 }
