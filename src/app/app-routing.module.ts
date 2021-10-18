afc02dde9ac72e4ed7baf0801dcc18e365900b96
yeah568 <awoo@dewdrop.dog>
2021-10-17 19:59:58 -0700

    worldmap routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ToastComponent } from './task/toast/toast.component';
+import { WorldmapComponent } from './worldmap/worldmap.component';
 
 const routes: Routes = [
+  {path: 'worldmap', component: WorldmapComponent},
   {path: 'task/toast', component: ToastComponent}
 ];
 
