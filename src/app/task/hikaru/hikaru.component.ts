682c358279098d0d3cdee63e84fe022e0c67fe23
yeah568 <awoo@dewdrop.dog>
2021-10-19 09:20:53 -0700

    add pieceTheme

--- src/app/task/hikaru/hikaru.component.ts
+++ src/app/task/hikaru/hikaru.component.ts
@@ -12,7 +12,10 @@ export class HikaruComponent implements OnInit {
 
   ngOnInit(): void {
     var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-    var board = Chessboard('myBoard', ruyLopez);
+    var board = Chessboard('myBoard', {
+      pieceTheme: 'assets/chesspieces/{piece}.png',
+      position: ruyLopez,
+    });
 
   }
 
